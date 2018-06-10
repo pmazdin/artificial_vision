4669dd09d203eba5d122fa3478c8c755404740ce
rojung <roland.jung@aau.at>
2018-06-10 17:36:04 +0200

    task: setup env file added.

---
+++ setup_env.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+ENV_NAME=my_py_env
+sudo apt-get install virtualenv
+
+export PYTHONPATH=
+#Then create and activate the virtual environment:
+virtualenv ${ENV_NAME}
+. ${ENV_NAME}/bin/activate
+# install the package (-e will keep the installed package up to date)
+pip install -r requirements.txt
+
+cd etc
+wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
+cd ..
